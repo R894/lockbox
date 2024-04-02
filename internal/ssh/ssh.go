04c9442c285d2e47bd54ab5a6f84497e4d53e104
R894 <kamil.khalaileh@gmail.com>
2024-04-03 01:01:16 +0300

    refactor: split handleRequest into more functions

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -12,25 +12,52 @@ import (
 	"github.com/gliderlabs/ssh"
 )
 
-func StartServer(tm *tunnel.TunnelManager) {
+type LockBox struct {
+	server *ssh.Server
+}
+
+func NewServer(addr string, tm *tunnel.TunnelManager) *LockBox {
 	ssh.Handle(func(s ssh.Session) {
-		handleRequest(s, tm)
+		handleRequest(s, nil)
 	})
-	log.Fatal(ssh.ListenAndServe(":2222", nil))
+	return &LockBox{server: &ssh.Server{
+		Addr: addr,
+		Handler: func(s ssh.Session) {
+			handleRequest(s, tm)
+		},
+	}}
+}
+
+func (l *LockBox) ListenAndServe() error {
+	return l.server.ListenAndServe()
+}
+
+func (l *LockBox) Close() error {
+	return l.server.Close()
 }
 
 func handleRequest(s ssh.Session, tm *tunnel.TunnelManager) {
 	id := rand.Intn(math.MaxInt)
 	tunnelChan := tm.AddTunnel(id)
+	tunnel := <-tunnelChan
+	defer close(tunnel.Donech)
 
 	s.Write(([]byte)(fmt.Sprintf("LockBox link: http://localhost:3000?id=%d\n", id)))
-	fmt.Println("tunnel is ready")
-	tunnel := <-tunnelChan
-	tunnel.Filename = s.RawCommand()
-	_, err := io.Copy(tunnel.W, s)
+	s.Write(([]byte)("Session is in progress... Waiting for user to connect and download\n"))
+	err := sendFileToTunnel(s.RawCommand(), s, &tunnel)
 	if err != nil {
 		log.Fatal(err)
+		s.Write(([]byte)("Something went wrong!\n"))
+		return
 	}
-	close(tunnel.Donech)
 	s.Write(([]byte)("File sent successfully, thanks for using LockBox!\n"))
+}
+
+func sendFileToTunnel(command string, session io.Reader, tunnel *tunnel.Tunnel) error {
+	tunnel.Filename = command
+	_, err := io.Copy(tunnel.W, session)
+	if err != nil {
+		return err
+	}
+	return nil
 }
