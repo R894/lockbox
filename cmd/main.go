121b790a48877757b4c8d89a98658dbfef8d36dd
R894 <kamil.khalaileh@gmail.com>
2024-04-04 02:03:13 +0300

    feat: add yaml settings to config

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
+
+	config "github.com/R894/lockbox"
 	"github.com/R894/lockbox/internal/ssh"
 	"github.com/R894/lockbox/internal/tunnel"
 	"github.com/R894/lockbox/internal/web"
 )
 
 func main() {
+	cfg := config.LoadConfig()
 	tm := tunnel.NewTunnelManager()
-	go func() { web.StartServer(tm) }()
-	ssh.NewServer("0.0.0.0:2222", tm).ListenAndServe()
+
+	go func() { web.StartServer(cfg.Web.Addr, tm) }()
+	log.Fatal(ssh.NewServer(cfg.SSH.Addr, tm).ListenAndServe())
 }
---
+++ config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type webserver struct {
+	Host string
+	Port int
+	Addr string
+}
+type ssh struct {
+	Host string
+	Port int
+	Addr string
+}
+
+type Config struct {
+	Web webserver
+	SSH ssh
+}
+
+func LoadConfig() *Config {
+	viperInit()
+	return &Config{
+		Web: webserver{
+			Host: viper.GetString("web.host"),
+			Port: viper.GetInt("web.port"),
+			Addr: fmt.Sprintf("%s:%d", viper.GetString("web.host"), viper.GetInt("web.port")),
+		},
+		SSH: ssh{
+			Host: viper.GetString("ssh.host"),
+			Port: viper.GetInt("ssh.port"),
+			Addr: fmt.Sprintf("%s:%d", viper.GetString("ssh.host"), viper.GetInt("ssh.port")),
+		},
+	}
+}
+
+func viperInit() {
+	setConfigProperties()
+	setViperDefaultValues()
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal("Error reading config file", err)
+	}
+}
+
+func setViperDefaultValues() {
+	viper.SetDefault("web.host", "localhost")
+	viper.SetDefault("web.port", 3000)
+	viper.SetDefault("ssh.host", "localhost")
+	viper.SetDefault("ssh.port", 2222)
+}
+
+func setConfigProperties() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+}
