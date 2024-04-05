bd222e4f0edd908252e16eaf0d609b54c74667f0
R894 <kamil.khalaileh@gmail.com>
2024-04-06 00:13:23 +0300

    refactor: make variable name clearer

--- internal/ssh/ssh_test.go
+++ internal/ssh/ssh_test.go
@@ -2,11 +2,43 @@ package ssh
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/R894/lockbox/internal/tunnel"
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/ssh"
 )
+
+func RunCmdOverSSH(addr, username, password, cmd string) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	cfg := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	conn, err := ssh.Dial("tcp", addr, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	session, err := conn.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		<-ctx.Done()
+		session.Close()
+	}()
+	return session.CombinedOutput(cmd)
+}
 
 func TestFileTransfer(t *testing.T) {
 	sessionData := []byte("Test data")
@@ -20,5 +52,17 @@ func TestFileTransfer(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, sessionData, tunnel.W.(*bytes.Buffer).Bytes())
-	assert.Equal(t, "test.txt", tunnel.Filename)
+}
+
+func TestStartServer(t *testing.T) {
+	fmt.Println("IN test")
+	tm := tunnel.NewTunnelManager()
+	srv := NewServer("127.0.0.1:2222", tm, "")
+	go srv.ListenAndServe()
+
+	res, _ := RunCmdOverSSH("127.0.0.1:2222", "root", "root", "test.txt")
+
+	resStr := string(res)
+
+	assert.Contains(t, resStr, "Session is in progress... Waiting for user to connect and download")
 }
